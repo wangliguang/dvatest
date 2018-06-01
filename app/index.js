980b4d0d9ae7e7e8dcf9864d8248223fd3a4b46c
王立广 <wangliguang-rj@ofo.com>
2018-06-01 16:29:18 +0800

    集成dva后的项目终于启动起来了

--- app/index.js
+++ app/index.js
@@ -1,3 +1,33 @@
 import HomePage from './container/HomePage';
-export default HomePage;
+import React from 'react';
+import { create } from 'dva-core';
+import { Provider } from 'react-redux';
 
+/* =============Model=============  */
+const UserModel = {
+  namespace: 'User',
+  state: {},
+  reducers: {
+    saveUser(state, { payload }) {
+      return {
+        ...state,
+        ...payload,
+      };
+    },
+  }
+}
+
+export default function () {
+  const app = create({
+    onError: (error) => { alert(error);},
+  });
+  app.model(UserModel);
+  app.start();
+
+  const store = app._store;
+  return (
+    <Provider store={store}>
+      <HomePage/>
+    </Provider>
+  )
+}
