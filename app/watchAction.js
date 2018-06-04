109cb2d1b4dedd9cf2726956ca037af6af230224
王立广 <wangliguang-rj@ofo.com>
2018-06-04 17:52:36 +0800

    fix: 先暂停打印日志

--- app/watchAction.js
+++ app/watchAction.js
@@ -1,12 +1,11 @@
 import { call, put, takeEvery, select } from 'redux-saga/effects'
 
 function* wathActionAndLogger() {
-  yield takeEvery('*', function* (action) {
-    const state = yield select()
-    console.log('action', action);
-    console.log('state', state);
-
-  });
+  // yield takeEvery('*', function* (action) {
+  //   const state = yield select()
+  //   console.log('action', action);
+  //   console.log('state', state);
+  // });
 }
 
 
